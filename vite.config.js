caa6a2758e809261a520da0e14ee07b1c0a1d203
avilrod3004 <avilrod3004@g.educaand.es>
2025-03-16 19:56:01 +0100

    browserlist

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,12 @@
 import { defineConfig } from 'vite';
 import tailwindcss from '@tailwindcss/vite'
+import browserslistToEsbuild from 'browserslist-to-esbuild'
 
 export default defineConfig({
     plugins: [
         tailwindcss(),
-    ]
+    ],
+    build: {
+        target: browserslistToEsbuild(),
+    },
 })
